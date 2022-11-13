ff4ecb4a39a664341de7dc64605414b3ad4b3645
Valle93 <valentin.eriksson@hotmail.se>
2022-11-13 22:55:47 +0100

    passed all post tests

--- server.js
+++ server.js
@@ -6,9 +6,66 @@ const server = app.listen(
     () => console.log('server open on ' + port)
 )
 
+const tooBig = 'must not be more than 6 digits'
+const notNumber = 'must be a number'
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var mapOfNumbers = new Map()
+var indexOfMap = 1
+
 app.get('/', (req, res) => {
 
     res.status(200).send('hello user')
+})
+
+function isItDigit(char){
+
+    for(var i = 0; i < numbers.length; i++){
+
+        if(char == numbers[i]){
+
+            return true
+        }
+    }
+
+    return false;
+}
+
+function isItNumber(input){
+
+    for(var i = 0; i < input.length; i++){
+
+        if(isItDigit(input.charAt(i)) == false){
+
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+app.post('/:number', (req, res) => {
+
+    const input = req.params.number;
+
+    if(input.length > 6){
+
+        return res.status(200).send(tooBig);
+    }
+
+    if(isItNumber(input) == false){
+
+        return res.status(200).send(notNumber)
+    }
+
+    const number = parseInt(input)
+
+    mapOfNumbers.set(indexOfMap, number)
+
+    indexOfMap++
+
+    return res.status(200).send(input)
+
 })
 
 module.exports = server;
--- test/homepage.test.js
+++ test/homepage.test.js
@@ -9,4 +9,26 @@ var request = require('supertest')
         })
     })
 
-    
+    describe('post', function () {
+        it('when user sends too big string', function (done) {
+            request(app).post('/123123123')
+            .expect(200)
+            .expect('must not be more than 6 digits', done)
+        })
+    })
+
+    describe('post', function () {
+        it('when user sends a string and not anumber', function (done) {
+            request(app).post('/123a')
+            .expect(200)
+            .expect('must be a number', done)
+        })
+    })
+
+    describe('post', function () {
+        it('when user sends an appropiate number', function (done) {
+            request(app).post('/123')
+            .expect(200)
+            .expect('123', done)
+        })
+    })
